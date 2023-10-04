43ac0448a866e95881d90033aa49649a1a388a2a
sgrimee <sgrimee@gmail.com.me>
2023-10-04 21:41:53 +0200

    fix: use hostname for client device name. Fix #4

--- src/teams/client.rs
+++ src/teams/client.rs
@@ -1,11 +1,15 @@
 // use super::{auth::get_integration_token, ClientCredentials};
+use gethostname::gethostname;
 use log::*;
 use oauth2::AccessToken;
 use webex::Webex;
 
 pub async fn get_webex_client(token: AccessToken) -> Webex {
     let secret: &str = token.secret();
-    let client = Webex::new(secret).await;
+    let device_name = gethostname()
+        .into_string()
+        .unwrap_or_else(|_| String::from("unknown"));
+    let client = Webex::new_with_device_name(&device_name, secret).await;
     debug!("Authenticated.");
     client
 }
